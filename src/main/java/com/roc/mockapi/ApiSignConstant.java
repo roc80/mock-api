d6bb1a00ab331d1971bfb0e027df2a661ac25ab6
roc <lipeng1080@gmail.com>
2026-02-26 08:41:31 +0800

    feat: 验签

---
+++ src/main/java/com/roc/mockapi/ApiSignConstant.java
@@ -0,0 +1,9 @@
+package com.roc.mockapi;
+
+/**
+ * @author roc
+ * @since 2026/2/26 8:34
+ */
+public class ApiSignConstant {
+    public static final Long REQUEST_VALID_MINUTES = 5L;
+}
--- src/main/java/com/roc/mockapi/controller/MockController.java
+++ src/main/java/com/roc/mockapi/controller/MockController.java
@@ -1,5 +1,9 @@
 package com.roc.mockapi.controller;
 
+import com.roc.apiclientsdk.module.response.ApiResponse;
+import com.roc.apiclientsdk.server.ApiServer;
+import com.roc.mockapi.ApiSignConstant;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +19,30 @@ import jakarta.servlet.http.HttpServletRequest;
  */
 @RestController
 @RequestMapping("/name")
+@AllArgsConstructor
 public class MockController {
+
+    private final ApiServer apiServer;
 
     @PostMapping("")
     public String name(@RequestBody User user, HttpServletRequest request) {
-
+        String nonce = request.getHeader("nonce");
+        String timestamp = request.getHeader("timestamp");
+        String bodyJson = request.getHeader("bodyJson");
+        String sign = request.getHeader("sign");
+        ApiResponse response = apiServer.verifySignature(sign, nonce, timestamp, bodyJson);
+        if (response.getCode() != 0) {
+            return response.getMessage();
+        }
+        /*防重放 begin*/
+        if (timestamp == null || timestamp.isEmpty()) {
+            return "Header: timestamp is null or empty";
+        } else if (System.currentTimeMillis() - Long.parseLong(timestamp) > ApiSignConstant.REQUEST_VALID_MINUTES * 60 * 1000) {
+            return "request was expired, more than " + ApiSignConstant.REQUEST_VALID_MINUTES + " minutes";
+        } else {
+            // todo nonce pool 实现
+        }
+        /*防重放 end*/
         return user.getUsername();
     }
 
